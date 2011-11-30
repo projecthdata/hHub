a8a365d2eda1858920dffee22a9b78fdd7dbbdd3
Eric Levine <levine.eric@gmail.com>
2011-11-30 13:35:30 -0500

    Added maven license plugin and generated appropriate license headers

---
+++ hHub-ehr-viewer/src/main/java/org/projecthdata/ehr/viewer/widget/WeightReadingListAdapter.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2011 The MITRE Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.projecthdata.ehr.viewer.widget;
+
+import java.util.List;
+
+import org.projecthdata.ehr.viewer.R;
+import org.projecthdata.ehr.viewer.model.WeightReading;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class WeightReadingListAdapter extends ArrayAdapter<WeightReading>{
+	LayoutInflater inflater = null;
+	
+	public WeightReadingListAdapter(Context context, List<WeightReading> readings){
+		super(context, R.layout.weight_list_item, readings);
+		this.inflater = LayoutInflater.from(getContext());
+		
+	}
+	
+	
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		if(convertView == null){
+			convertView = inflater.inflate(R.layout.weight_list_item, null);
+		}
+		WeightReading reading = (WeightReading)getItem(position);
+		((TextView)convertView.findViewById(R.id.weight_list_item_value_text)).setText(reading.getResultValue());
+		return convertView;
+	}
+}
